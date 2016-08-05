61f98f5eec0d5ed6e28924c734eca8df030355fb
Ivar Nilsen <post@ivarnilsen.com>
2016-08-05 17:54:55 +0200

    Install node and sublimetext

--- provision/provision-post.sh
+++ provision/provision-post.sh
@@ -6,3 +6,13 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 sudo sed -i 's/google\-chrome\-stable/google\-chrome\-stable \-\-disable-gpu/g' /usr/share/applications/google-chrome.desktop
+
+echo -e "\n**** Installing Sublime Text"
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install -y sublime-text-installer
+
+echo -e "\n**** Installing node"
+sudo apt-get install -y npm
+sudo npm install -g n
+sudo n latest
